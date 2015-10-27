1afc9864e462dd2e19ec669ff81d828df6bf5e09
Niklas Persson <valekhz@gmail.com>
2015-10-27 21:23:57 +0100

    Rooks

--- src/com/jchess/pieces/Rook.java
+++ src/com/jchess/pieces/Rook.java
@@ -6,17 +6,72 @@ import com.jchess.game.Color;
 import com.jchess.move.Move;
 import com.jchess.game.Piece;
 import com.jchess.game.PieceType;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
+    private boolean captureMove;
+
     public Rook(Color color) {
         super(PieceType.ROOK, color);
     }
 
     @Override
-    public List<Move> validMoves(Chessboard board, Square current) {
-        throw new NotImplementedException();
+    public List<Move> validMoves(Chessboard board, Square src) {
+        List<Move> moves = new ArrayList<>();
+
+        Square dest;
+
+        // Forward
+        captureMove = false;
+        dest = src.forward(color);
+        while (canMove(board, dest)) {
+            moves.add(new Move(board, src, dest));
+            dest = dest.forward(color);
+            if (captureMove)
+                break;
+        }
+
+        // Backward
+        captureMove = false;
+        dest = src.backward(color);
+        while (canMove(board, dest)) {
+            moves.add(new Move(board, src, dest));
+            dest = dest.backward(color);
+            if (captureMove)
+                break;
+        }
+
+        // Left
+        captureMove = false;
+        dest = src.left(color);
+        while (canMove(board, dest)) {
+            moves.add(new Move(board, src, dest));
+            dest = dest.left(color);
+            if (captureMove)
+                break;
+        }
+
+        // Left
+        captureMove = false;
+        dest = src.right(color);
+        while (canMove(board, dest)) {
+            moves.add(new Move(board, src, dest));
+            dest = dest.right(color);
+            if (captureMove)
+                break;
+        }
+
+        return moves;
+    }
+
+    private boolean canMove(Chessboard board, Square square) {
+        Piece piece = board.getPiece(square);
+
+        if (piece != null && piece.getColor() != color)
+            captureMove = true;
+
+        return square.isValid() && (piece == null || piece.getColor() != color);
     }
 }
